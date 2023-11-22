29c70a8423856f4749e144eb24e9a13bdd221ae5
Meehir Lokhande <116459433+meehirlokhande@users.noreply.github.com>
2023-11-22 22:54:23 +0530

    Create print-1-to-n-without-using-loops.cpp

---
+++ print-1-to-n-without-using-loops.cpp
@@ -0,0 +1,16 @@
+https://www.geeksforgeeks.org/problems/print-1-to-n-without-using-loops-1587115620/1?page=2&difficulty=School,Basic&sortBy=submissions
+
+class Solution{
+    public:
+    //Complete this function
+    void printNos(int N)
+    {
+        int count=1;
+        while(count<=N)
+        {
+            cout<<count<<" ";
+            count++;
+        }
+        
+    }
+};
