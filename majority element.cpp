333134082f4d5e8633a1ab6b1fc7f57bb43fbb13
Meehir Lokhande <116459433+meehirlokhande@users.noreply.github.com>
2023-12-09 11:29:18 +0530

    majority element.cpp

--- majority element.cpp
+++ majority element.cpp
@@ -34,3 +34,38 @@ class Solution{
         
     }
 };
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+
+#include <unordered_map>
+class Solution{
+  public:
+     // Function to find majority element in the array
+    // a: input array
+    // size: size of input array
+    int majorityElement(int a[], int size)
+    {
+        unordered_map<int,int> umap;
+        for(int i=0;i<size;i++)
+        {
+            if(umap.find(a[i])!=umap.end()){
+                umap[a[i]]++;
+            }else{
+                umap[a[i]]=1;
+            }
+        }
+        
+        for(auto it : umap){
+            if(it.second> size/2){
+                return it.first;
+            }
+        }
+        
+        return -1;
+        
+        
+        
+        
+    }
+};
+
